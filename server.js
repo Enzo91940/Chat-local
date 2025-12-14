da6fb87f43b7e401531240e9df48e65de9607d0b
Enzo91940 <enzo.vajoumouny01@gmail.com>
2025-12-14 16:29:30 +0100

    commit

--- server.js
+++ server.js
@@ -4,64 +4,95 @@ const socketIo = require('socket.io');
 const fs = require('fs');
 const path = require('path');
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const application = express();
+const serveurHttp = http.createServer(application);
+const io = socketIo(serveurHttp);
 const PORT = 3002;
 
-const USERS_FILE = path.join(__dirname, 'users.json');
-if (!fs.existsSync(USERS_FILE)) fs.writeFileSync(USERS_FILE, '{}');
+/*Gestion des utilisateurs  */
 
-const loadUsers = () => JSON.parse(fs.readFileSync(USERS_FILE));
-const saveUsers = (users) => fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2));
+const FICHIER_UTILISATEURS = path.join(__dirname, 'utilisateurs.json');
+if (!fs.existsSync(FICHIER_UTILISATEURS)) {
+  fs.writeFileSync(FICHIER_UTILISATEURS, '{}');
+}
 
-app.use(express.static(path.join(__dirname, 'public')));
+const chargerUtilisateurs = () =>
+  JSON.parse(fs.readFileSync(FICHIER_UTILISATEURS));
 
-// Redirection racine vers le login
-app.get('/', (req, res) => {
-  res.redirect('/login.html');
+const sauvegarderUtilisateurs = (utilisateurs) =>
+  fs.writeFileSync(
+    FICHIER_UTILISATEURS,
+    JSON.stringify(utilisateurs, null, 2)
+  );
+
+/*Configuration Express*/
+
+application.use(express.static(path.join(__dirname, 'public')));
+
+// Redirection racine vers la page de connexion
+application.get('/', (requete, reponse) => {
+  reponse.redirect('/login.html');
 });
 
+/*Socket.IO */
+
 io.on('connection', (socket) => {
-  socket.on('register', ({ username, password }) => {
-    const users = loadUsers();
-    if (users[username]) {
-      socket.emit('register error', 'Utilisateur déjà existant.');
+
+  socket.on('inscription', ({ nomUtilisateur, motDePasse }) => {
+    const utilisateurs = chargerUtilisateurs();
+
+    if (utilisateurs[nomUtilisateur]) {
+      socket.emit('erreur inscription', 'Utilisateur déjà existant.');
     } else {
-      users[username] = { password };
-      saveUsers(users);
-      socket.emit('register success');
+      utilisateurs[nomUtilisateur] = { motDePasse };
+      sauvegarderUtilisateurs(utilisateurs);
+      socket.emit('succes inscription');
     }
   });
 
-  socket.on('login', ({ username, password }) => {
-    const users = loadUsers();
-    if (!users[username] || users[username].password !== password) {
-      socket.emit('login error', 'Identifiants invalides.');
+  socket.on('connexion', ({ nomUtilisateur, motDePasse }) => {
+    const utilisateurs = chargerUtilisateurs();
+
+    if (
+      !utilisateurs[nomUtilisateur] ||
+      utilisateurs[nomUtilisateur].motDePasse !== motDePasse
+    ) {
+      socket.emit('erreur connexion', 'Identifiants invalides.');
     } else {
-      socket.username = username;
-      socket.emit('login success', username);
-      io.emit('chat message', `${username} a rejoint le chat.`);
+      socket.nomUtilisateur = nomUtilisateur;
+      socket.emit('succes connexion', nomUtilisateur);
+      io.emit('message chat', `${nomUtilisateur} a rejoint le chat.`);
     }
   });
 
-  socket.on('reconnect username', (username) => {
-    socket.username = username;
+  socket.on('reconnexion utilisateur', (nomUtilisateur) => {
+    socket.nomUtilisateur = nomUtilisateur;
   });
 
-  socket.on('chat message', (msg) => {
-    if (socket.username) {
-      io.emit('chat message', `${socket.username} : ${msg}`);
+  socket.on('message chat', (message) => {
+    if (socket.nomUtilisateur) {
+      io.emit(
+        'message chat',
+        `${socket.nomUtilisateur} : ${message}`
+      );
     } else {
-      socket.emit('chat message', 'Veuillez vous connecter.');
+      socket.emit('message chat', 'Veuillez vous connecter.');
     }
   });
 
   socket.on('disconnect', () => {
-    if (socket.username) {
-      io.emit('chat message', `${socket.username} a quitté le chat.`);
+    if (socket.nomUtilisateur) {
+      io.emit(
+        'message chat',
+        `${socket.nomUtilisateur} a quitté le chat.`
+      );
     }
   });
+
 });
 
-server.listen(PORT, () => console.log(`Serveur en écoute sur http://localhost:${PORT}`));
+/* Démarrage du serveur */
+
+serveurHttp.listen(PORT, () => {
+  console.log(`Serveur en écoute sur http://localhost:${PORT}`);
+});
